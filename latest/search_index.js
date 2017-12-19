1b80f1dd12dbcf385893670b90c5bf0e132bf712
autodocs <autodocs>
2017-12-19 11:17:05 +0000

    build based on 127a694

---
+++ latest/search_index.js
@@ -0,0 +1,59 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Documentation-1",
+    "page": "Home",
+    "title": "Documentation",
+    "category": "section",
+    "text": "TODO"
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "To install the package execute the following command in the REPL:Pkg.clone(\"https://github.com/crstnbr/MonteCarlo.jl\")Afterwards, you can use MonteCarlo.jl like any other package installed with Pkg.add():using MonteCarloTo obtain the latest version of the package just do Pkg.update() or specifically Pkg.update(\"MonteCarlo\")."
+},
+
+{
+    "location": "functions.html#",
+    "page": "Functions",
+    "title": "Functions",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "functions.html#Functions-1",
+    "page": "Functions",
+    "title": "Functions",
+    "category": "section",
+    "text": "Below you will find all methods exported by MonteCarlo.jl."
+},
+
+{
+    "location": "functions.html#Index-1",
+    "page": "Functions",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "functions.html#Docs-1",
+    "page": "Functions",
+    "title": "Docs",
+    "category": "section",
+    "text": "Modules = [MonteCarlo]\nPrivate = false\nOrder   = [:function, :type]"
+},
+
+]}
