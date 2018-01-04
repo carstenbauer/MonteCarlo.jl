b1bb65647c00c9ada50e03dc0a2d76b99a0ab56e
autodocs <autodocs>
2018-01-04 00:03:22 +0000

    build based on e3ce889

--- latest/search_index.js
+++ latest/search_index.js
@@ -93,7 +93,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Custom models",
     "title": "Custom models",
     "category": "section",
-    "text": "Although MonteCarlo.jl already ships with famous models, foremost the Ising and Hubbard models, a key idea of the design of the package is to have a (rather) well defined interface between models and Monte Carlo flavors. This way it should be easy for you to implement your own physical model. Sometimes examples say more than words, so feel encouraged to have a look at the implementations of the above mentioned models."
+    "text": "Although MonteCarlo.jl already ships with famous models, foremost the Ising and Hubbard models, a key idea of the design of the package is to have a (rather) well defined interface between models and Monte Carlo flavors. This way it should be easy for you to extend the package and implement your own physical model. Sometimes examples say more than words, so feel encouraged to have a look at the implementations of the above mentioned models."
+},
+
+{
+    "location": "manual/custommodels.html#Semantics-1",
+    "page": "Custom models",
+    "title": "Semantics",
+    "category": "section",
+    "text": "Loosely speeking, we define a Model to be a Hamiltonian on a lattice. Therefore, the lattice is part of a model. The motivation for this modeling is that the physics of a system does not only depend on the Hamiltonian but also (sometime drastically) on the underlying lattice. This is for example very obvious for spin systems which due to the lattice might become (geometrically) frustrated and show spin liquids physics. Also, from a technical point of view, lattice information is almost exclusively processed in energy calculations which both relate to the Hamiltonian (and therefore the model).note: Note\nWe will generally use the terminology Hamiltonian, energy and so on. However, this doesn't restrict you from defining your model as an Lagrangian with an action in any way as this just corresponds to a one-to-one mapping of interpretations."
 },
 
 {
@@ -101,7 +109,39 @@ var documenterSearchIndex = {"docs": [
     "page": "Custom models",
     "title": "Mandatory fields and methods",
     "category": "section",
-    "text": "Any concrete model type, let's call it MyModel in the following, must be a subtype of the abstract type MonteCarlo.Model. Internally it must have at least the following fields:β::Float64: inverse temperature\nl::Lattice: any LatticeFurthermore it must implement the following methods:conftype: type of a configuration\nenergy: energy of configuration\nrand: random configuration\npropose_local: propose local move\naccept_local: accept a local moveA full list of methods that should be implemented for MyModel can be found here: Methods: Models."
+    "text": "Any concrete model (type), let's call it MyModel in the following, must be a subtype of the abstract type MonteCarlo.Model. To work with a Monte Carlo flavor, it must internally have at least the following fields:β::Float64: inverse temperature\nl::Lattice: any LatticeFurthermore it must implement the following methods:conftype: type of a configuration\nenergy: energy of configuration\nrand: random configuration\npropose_local: propose local move\naccept_local: accept a local moveA full list of methods with precise signatures that should be implemented for MyModel can be found here: Methods: Models."
+},
+
+{
+    "location": "manual/custommodels.html#Lattice-requirements-1",
+    "page": "Custom models",
+    "title": "Lattice requirements",
+    "category": "section",
+    "text": "The Hamiltonian of your model might impose some requirements on the Lattice object that you use as it must provide you with enough lattice information.It might be educating to look at the structure of the simple SquareLattice struct. mutable struct SquareLattice <: CubicLattice\n    L::Int\n    sites::Int\n    neighs::Matrix{Int} # row = up, right, down, left; col = siteidx\n    neighs_cartesian::Array{Int, 3} # row (1) = up, right, down, left; cols (2,3) = cartesian siteidx\n    sql::Matrix{Int}\n    SquareLattice() = new()\nendIt only provides access to next nearest neighbors through the arrays neighs and neighs_cartesian. If your model's Hamiltonian requires higher order neighbor information, because of, let's say, a next next nearest neighbor hopping term, the SquareLattice doesn't suffice. You could either extend this Lattice or implement a NNSquareLattice for example."
+},
+
+{
+    "location": "manual/customlattices.html#",
+    "page": "Custom lattices",
+    "title": "Custom lattices",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "manual/customlattices.html#Custom-lattices-1",
+    "page": "Custom lattices",
+    "title": "Custom lattices",
+    "category": "section",
+    "text": "As described in Custom models a lattice is considered to be part of a model. Hence, most of the requirements for fields of a Lattice subtype come from potential models (see Lattice requirements). Below you'll find information on which fields are mandatory from a Monte Carlo flavor point of view."
+},
+
+{
+    "location": "manual/customlattices.html#Mandatory-fields-and-methods-1",
+    "page": "Custom lattices",
+    "title": "Mandatory fields and methods",
+    "category": "section",
+    "text": "Any concrete lattice type, let's call it MyLattice in the following, must be a subtype of the abstract type MonteCarlo.Lattice. To work with a Monte Carlo flavor, it must internally have at least have the following field,sites: number of lattice sites.However, as already mentioned above depending on the physical model of interest it will typically also have (at least) something likeneighs: next nearest neighbors,as most Hamiltonian will need next nearest neighbor information.The only reason why such a field isn't generally mandatory is that the Monte Carlo routine doesn't care about the lattice much. Neighbor information is usually only used in the energy (difference) calculation of a particular configuration like done in energy or propose_local which both belong to a Model."
 },
 
 {
@@ -197,7 +237,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Models",
     "title": "Methods: Models",
     "category": "section",
-    "text": "Below you find all methods that any particular physical model (subtype of the abstract type MonteCarlo.Model) should implement. See Custom models for more information."
+    "text": "Below you find all methods that any particular physical model (subtype of the abstract type MonteCarlo.Model) should implement. See also Custom models for more information."
 },
 
 {
@@ -213,7 +253,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Models",
     "title": "MonteCarlo.observables",
     "category": "Method",
-    "text": "observables(m::Model)\n\nGet a list of all observables defined for a given model.\n\nReturns a Dict{String, String} where values are the observables names and keys are short versions of those names. The keys can be used to collect correponding observable objects from a Monte Carlo simulation, e.g. like mc.obs[key].\n\n\n\n"
+    "text": "observables(m::Model)\n\nGet a list of all observables defined for a given model.\n\nReturns a Dict{String, String} where values are the observables names and keys are short versions of those names. The keys can be used to collect correponding observable objects from a Monte Carlo simulation, e.g. like mc.obs[key].\n\nNote, there is no need to implement this function for a custom model.\n\n\n\n"
 },
 
 {
