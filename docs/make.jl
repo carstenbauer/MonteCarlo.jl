228f9e711284842498fdff2664c4af230934b130
Carsten Bauer <crstnbr@gmail.com>
2018-03-29 18:29:26 +0200

    mkdocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,39 +1,11 @@
 using Documenter, MonteCarlo
 
-makedocs(
-    modules = [MonteCarlo],
-    sitename = "MonteCarlo.jl",
-    pages = [
-        "Home" => "index.md",
-        "Manual" => [
-            "Getting started" => "manual/gettingstarted.md",
-            "Examples" => "manual/examples.md"
-        ],
-        "Physical models" => [
-            "Ising model" => "models/ising.md",
-            "Attractive Hubbard model" => "models/hubbardattractive.md"
-        ],
-        "Monte Carlo flavors" => [
-            "MC" => "flavors/mc.md",
-            "DQMC" => "flavors/dqmc.md"
-        ],
-        "Lattices" => "lattices.md",
-        "Customize" => "customize.md",
-        "Methods" => [
-            "General" => "methods/general.md"
-        ],
-        "Interfaces" => [
-            "MC" => "interfaces/MC.md",
-            "DQMC" => "interfaces/DQMC.md"
-        ]
-    ]
-    # assets = ["assets/custom.css", "assets/custom.js"]
-)
+makedocs(modules = [MonteCarlo], doctest=false)
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math", 
+        "pygments", "pymdown-extensions"),
     repo   = "github.com/crstnbr/MonteCarlo.jl.git",
     julia  = "release",
     osname = "linux",
-    target = "build"
 )
