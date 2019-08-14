e0fc0007998582cc1866dc6632aa770502289cba
Carsten Bauer <crstnbr@gmail.com>
2019-08-15 00:19:47 +0200

    close #10; add MC print_rate; Int8->Int64 IsingSpin

--- src/flavors/MC/MC_mandatory.jl
+++ src/flavors/MC/MC_mandatory.jl
@@ -5,47 +5,34 @@ Number of lattice sites of the given model.
 """
 nsites(m::Model) = error("Model has no implementation of `nsites(m::Model)`!")
 
-"""
-    conftype(::Type{MC}, m::Model)
-
-Returns the type of a configuration.
-"""
-conftype(::Type{MC}, m::Model) = error("Model has no implementation of `conftype(::Type{MC}, m::Model)`!")
 
 """
-    energy(mc::MC, m::Model, conf)
-
-Calculate energy of configuration `conf` for Model `m`.
-"""
-energy(mc::MC, m::Model, conf) = error("Model has no implementation of `energy(mc::MC, m::Model, conf)`!")
-
-import Base.rand
-"""
-    rand(mc::MC, m::Model)
+    rand(m::Model)
 
 Draw random configuration.
 """
-rand(mc::MC, m::Model) = error("Model has no implementation of `rand(mc::MC, m::Model)`!")
+Base.rand(m::Model) = error("Model has no implementation of `rand(m::Model)`!")
+
 
 """
-    propose_local(mc::MC, m::Model, i::Int, conf, E::Float64) -> delta_E, delta_i
+    propose_local(mc::MC, m::Model, i::Int, conf) -> delta_E, delta_i
 
-Propose a local move for lattice site `i` of current configuration `conf`
-with energy `E`. Returns local move information `delta_i`
-(e.g. `new[i] - conf[i]`, will be forwarded to `accept_local!`) and energy
-difference `delta_E = E_new - E_old`.
+Propose a local move for lattice site `i` of current configuration `conf`.
+Returns local move information `delta_i` (e.g. `new[i] - conf[i]`, will be
+forwarded to `accept_local!`) and energy difference `delta_E = E_new - E_old`.
 
 See also [`accept_local!`](@ref).
 """
-propose_local(mc::MC, m::Model, i::Int, conf, E::Float64) = error("Model has no implementation of `propose_local(mc::MC, m::Model, i::Int, conf, E::Float64)`!")
+propose_local(mc::MC, m::Model, i::Int, conf) = error("Model has no implementation of `propose_local(mc::MC, m::Model, i::Int, conf)`!")
+
 
 """
-    accept_local(mc::MC, m::Model, i::Int, conf, E::Float64, delta_i, delta_E::Float64)
+    accept_local(mc::MC, m::Model, i::Int, conf, delta_i, delta_E::Float64)
 
-Accept a local move for site `i` of current configuration `conf`
-with energy `E`. Arguments `delta_i` and `delta_E` correspond to output of `propose_local()`
+Accept a local move for site `i` of current configuration `conf`.
+Arguments `delta_i` and `delta_E` correspond to output of `propose_local()`
 for that local move.
 
 See also [`propose_local`](@ref).
 """
-accept_local!(mc::MC, m::Model, i::Int, conf, E::Float64, delta_i, delta_E::Float64) = error("Model has no implementation of `accept_local!(m::Model, i::Int, conf, E::Float64, delta_i, delta_E::Float64)`!")
+accept_local!(mc::MC, m::Model, i::Int, conf, delta_i, delta_E::Float64) = error("Model has no implementation of `accept_local!(m::Model, i::Int, conf, delta_i, delta_E::Float64)`!")
--- src/models/Ising/IsingModel.jl
+++ src/models/Ising/IsingModel.jl
@@ -1,4 +1,6 @@
-const IsingSpin = Int8 # can't use something more efficient here because of bug in MonteCarloObservable (see #10 on gitsrv)
+import Base: @propagate_inbounds
+
+const IsingSpin = Int64
 const IsingDistribution = IsingSpin[-1,1]
 const IsingConf = Array{IsingSpin}
 
@@ -41,6 +43,9 @@ function IsingModel(kwargs::Dict{Symbol, Any})
     IsingModel(; kwargs...)
 end
 
+# convenience
+@inline Base.ndims(m::IsingModel) = m.dims
+
 # cosmetics
 import Base.summary
 import Base.show
@@ -49,93 +54,40 @@ Base.show(io::IO, model::IsingModel) = print(io, "$(model.dims)D-Ising model, L=
 Base.show(io::IO, m::MIME"text/plain", model::IsingModel) = print(io, model)
 
 
-# methods for using it with Monte Carlo flavor MC (Monte Carlo)
-"""
-    nsites(m::IsingModel)
 
-Number of sites of the given Ising model.
-Propagates `nsites` to the underlying `Lattice`.
-"""
+
+# mandatory methods for Monte Carlo flavor `MC`
 @inline nsites(m::IsingModel) = nsites(m.l)
+Base.rand(m::IsingModel) = rand(IsingDistribution, fill(m.L, ndims(m))...)
 
-"""
-    energy(mc::MC, m::IsingModel, conf::IsingConf)
-
-Calculate energy of Ising configuration `conf` for Ising model `m`.
-"""
-function energy(mc::MC, m::IsingModel, conf::IsingConf)
-  neigh = m.l.neighs
-    E = 0.0
-    for n in 1:m.dims
-        @inbounds @simd for i in 1:m.l.sites
-            E -= conf[i]*conf[neigh[n,i]]
-        end
-    end
-    return E
-end
-
-"""
-    energy(mc::MC, m::IsingModel{SquareLattice}, conf::IsingConf)
-
-Calculate energy of Ising configuration `conf` for 2D Ising model `m`.
-This method is a faster variant of the general method for the square lattice case.
-(It is roughly twice as fast in this case.)
-"""
-function energy(mc::MC, m::IsingModel{SquareLattice}, conf::IsingConf)
-  neigh = m.l.neighs
-    E = 0.0
-    @inbounds @simd for i in 1:m.l.sites
-        E -= conf[i]*conf[neigh[1,i]] + conf[i]*conf[neigh[2,i]]
-    end
-    return E
-end
-
-import Base.rand
-"""
-    rand(mc::MC, m::IsingModel)
-
-Draw random Ising configuration.
-"""
-rand(mc::MC, m::IsingModel) = rand(IsingDistribution, fill(m.L, m.dims)...)
-
-"""
-    conftype(::Type{MC}, m::IsingModel)
-
-Returns the type of an Ising model configuration.
-"""
-conftype(::Type{MC}, m::IsingModel) = Array{IsingSpin, m.dims}
-
-"""
-    propose_local(mc::MC, m::IsingModel, i::Int, conf::IsingConf, E::Float64) -> delta_E, delta_i
-
-Propose a local spin flip at site `i` of current configuration `conf`
-with energy `E`. Returns the local move `delta_i = new[i] - conf[i]` and energy difference `delta_E = E_new - E_old`.
-"""
-@inline function propose_local(mc::MC, m::IsingModel, i::Int, conf::IsingConf, E::Float64)
+@propagate_inbounds function propose_local(mc::MC, m::IsingModel, i::Int, conf::IsingConf)
     delta_E = 2. * conf[i] * sum(conf[m.l.neighs[:,i]])
-    return delta_E, conf[i]==1 ? -2 : 2
+    return delta_E, nothing
 end
 
-"""
-    accept_local(mc::MC, m::IsingModel, i::Int, conf::IsingConf, E::Float64, delta_i, delta_E::Float64)
-
-Accept a local spin flip at site `i` of current configuration `conf`
-with energy `E`. Arguments `delta_i` and `delta_E` correspond to output of `propose_local()`
-for that spin flip.
-"""
-@inline function accept_local!(mc::MC, m::IsingModel, i::Int, conf::IsingConf, E::Float64, delta_i, delta_E::Float64)
+@propagate_inbounds function accept_local!(mc::MC, m::IsingModel, i::Int, conf::IsingConf, delta_i, delta_E::Float64)
     conf[i] *= -1
     nothing
 end
 
-"""
-    global_move(mc::MC, m::IsingModel, conf::IsingConf, E::Float64) -> accepted::Bool
+# optimized for 2D case
+@propagate_inbounds function propose_local(mc::MC, m::IsingModel{SquareLattice}, i::Int, conf::IsingConf)
+    neighs = m.l.neighs
+    @inbounds delta_E = 2. * conf[i] * (conf[neighs[1, i]] + conf[neighs[2, i]] +
+                              + conf[neighs[3, i]] + conf[neighs[4, i]])
+    return delta_E, nothing
+end
 
-Constructs a Wolff cluster spinflip for configuration `conf` with energy `E`.
+
+# optional functions/methods
+"""
+    global_move(mc::MC, m::IsingModel, conf::IsingConf) -> accepted::Bool
+
+Constructs a Wolff cluster spinflip for configuration `conf`.
 Returns wether a cluster spinflip has been performed (any spins have been flipped).
 """
-function global_move(mc::MC, m::IsingModel, conf::IsingConf, E::Float64)
-  N = m.l.sites
+function global_move(mc::MC, m::IsingModel, conf::IsingConf)
+  N = nsites(m)
   neighs = m.l.neighs
   beta = mc.p.beta
 
@@ -163,19 +115,12 @@ function global_move(mc::MC, m::IsingModel, conf::IsingConf, E::Float64)
     end
 
     return length(cluster)>1
-    #return length(cluster) # technically a misuse. allows us to see the cluster size * 100 as acc_rate_global.
 end
 
-"""
-    prepare_observables(mc::MC, m::IsingModel)
 
-Initializes observables for the Ising model and returns a `Dict{String, Observable}`.
-
-See also [`measure_observables!`](@ref) and [`finish_observables!`](@ref).
-"""
 @inline function prepare_observables(mc::MC, m::IsingModel)
     obs = Dict{String,Observable}()
-    obs["confs"] = Observable(conftype(MC, m), "Configurations")
+    obs["confs"] = Observable(typeof(mc.conf), "Configurations")
 
     obs["E"] = Observable(Float64, "Total energy")
     obs["E2"] = Observable(Float64, "Total energy squared")
@@ -192,19 +137,47 @@ See also [`measure_observables!`](@ref) and [`finish_observables!`](@ref).
     return obs
 end
 
+
 """
-    measure_observables!(mc::MC, m::IsingModel, obs::Dict{String,Observable}, conf::IsingConf, E::Float64)
+    energy(mc::MC, m::IsingModel, conf::IsingConf)
 
-Measures observables and updates corresponding `Observable` objects in `obs`.
-
-See also [`prepare_observables`](@ref) and [`finish_observables!`](@ref).
+Calculate energy of Ising configuration `conf` for Ising model `m`.
 """
-@inline function measure_observables!(mc::MC, m::IsingModel, obs::Dict{String,Observable}, conf::IsingConf, E::Float64)
-  N = m.l.sites
+function energy(mc::MC, m::IsingModel, conf::IsingConf)
+    neigh = m.l.neighs
+    E = 0.0
+    for n in 1:ndims(m)
+        @inbounds @simd for i in 1:nsites(m)
+            E -= conf[i]*conf[neigh[n,i]]
+            @show E
+        end
+    end
+    return E
+end
 
+"""
+    energy(mc::MC, m::IsingModel{SquareLattice}, conf::IsingConf)
+
+Calculate energy of Ising configuration `conf` for 2D Ising model `m`.
+This method is a faster variant of the general method for the
+square lattice case. (It is roughly twice as fast in this case.)
+"""
+function energy(mc::MC, m::IsingModel{SquareLattice}, conf::IsingConf)
+    neigh = m.l.neighs
+    E = 0.0
+    @inbounds @simd for i in 1:nsites(m)
+        E -= conf[i]*conf[neigh[1,i]] + conf[i]*conf[neigh[2,i]]
+    end
+    return E
+end
+
+
+@inline function measure_observables!(mc::MC, m::IsingModel, obs::Dict{String,Observable}, conf::IsingConf)
+    N = nsites(m)
     push!(obs["confs"], conf)
 
     # energie
+    E = energy(mc, m, conf)
     E2 = E^2
     push!(obs["E"], E)
     push!(obs["E2"], E2)
@@ -216,30 +189,28 @@ See also [`prepare_observables`](@ref) and [`finish_observables!`](@ref).
     push!(obs["M"], M)
     push!(obs["M2"], M2)
     push!(obs["m"], M/N)
-
     nothing
 end
 
 """
-    measure_observables!(mc::MC, m::IsingModel, obs::Dict{String,Observable}, conf::IsingConf, E::Float64)
+    measure_observables!(mc::MC, m::IsingModel, obs::Dict{String,Observable}, conf::IsingConf)
 
 Calculates magnetic susceptibility and specific heat and updates corresponding `Observable` objects in `obs`.
 
 See also [`prepare_observables`](@ref) and [`measure_observables!`](@ref).
 """
 @inline function finish_observables!(mc::MC, m::IsingModel, obs::Dict{String,Observable})
-  N = m.l.sites
-  beta = mc.p.beta
+    N = nsites(m)
+    beta = mc.p.beta
 
     # specific heat
-  E = mean(obs["E"])
-  E2 = mean(obs["E2"])
+    E = mean(obs["E"])
+    E2 = mean(obs["E2"])
     push!(obs["C"], beta*beta*(E2/N - E*E/N))
 
     # susceptibility
-  M = mean(obs["M"])
-  M2 = mean(obs["M2"])
+    M = mean(obs["M"])
+    M2 = mean(obs["M2"])
     push!(obs["χ"], beta*(M2/N - M*M/N))
-
     nothing
 end
