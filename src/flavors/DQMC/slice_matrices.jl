0f9f1624313025cf4224cab9689e78070af8b95f
Frederic Freyer <frederic481994@hotmail.de>
2019-08-21 18:03:14 +0200

    in-place slice matrix multiplications (#9)
    * rename stack.udt_tmp to stack.tmp
    
    * add in-place slice_matrix!(::DQMC_CBFalse, ...)
    
    * change to in-place multiply_slice_matrix... for DQMC_CBFalse
    
    * add in-place slice_matrix!(::DQMC_CBTrue, ...), fix deprecation
    
    * change to in-place multiply_slice_matrix for DQMC_CBTrue
    
    * add tests for slice matrices
    
    * add right_mul! workaround
    
    * change right_mul! to mul!,  maximum(abs(A-B)) to maximum(abs, A-B)
    
    * add warning if mul! already defined

--- src/flavors/DQMC/slice_matrices.jl
+++ src/flavors/DQMC/slice_matrices.jl
@@ -21,29 +21,60 @@ function slice_matrix(mc::DQMC_CBFalse, m::Model, slice::Int,
 		return eV * eTinv * eTinv
 	end
 end
+function slice_matrix!(mc::DQMC_CBFalse, m::Model, slice::Int,
+					power::Float64=1., result = mc.s.U)
+	eT = mc.s.hopping_matrix_exp
+	eTinv = mc.s.hopping_matrix_exp_inv
+	eV = mc.s.eV
+
+	interaction_matrix_exp!(mc, m, eV, mc.conf, slice, power)
+
+	if power > 0
+		# eT * (eT * eV)
+		mul!(mc.s.tmp, eT, eV)
+		mul!(result, eT, mc.s.tmp)
+	else
+		# ev * (eTinv * eTinv)
+		mul!(mc.s.tmp, eTinv, eTinv)
+		mul!(result, eV, mc.s.tmp)
+	end
+	return result
+end
+
+
 function multiply_slice_matrix_left!(mc::DQMC_CBFalse, m::Model,
 								slice::Int, M::AbstractMatrix)
-	M .= slice_matrix(mc, m, slice, 1.) * M
+	slice_matrix!(mc, m, slice, 1.0, mc.s.U)
+	mul!(mc.s.tmp, mc.s.U, M)
+	M .= mc.s.tmp
 	nothing
 end
 function multiply_slice_matrix_right!(mc::DQMC_CBFalse, m::Model,
 								slice::Int, M::AbstractMatrix)
-	M .= M * slice_matrix(mc, m, slice, 1.)
+	slice_matrix!(mc, m, slice, 1.0, mc.s.U)
+	mul!(mc.s.tmp, M, mc.s.U)
+	M .= mc.s.tmp
 	nothing
 end
 function multiply_slice_matrix_inv_right!(mc::DQMC_CBFalse, m::Model,
 								slice::Int, M::AbstractMatrix)
-	M .= M * slice_matrix(mc, m, slice, -1.)
+	slice_matrix!(mc, m, slice, -1.0, mc.s.U)
+	mul!(mc.s.tmp, M, mc.s.U)
+	M .= mc.s.tmp
 	nothing
 end
 function multiply_slice_matrix_inv_left!(mc::DQMC_CBFalse, m::Model,
 								slice::Int, M::AbstractMatrix)
-	M .= slice_matrix(mc, m, slice, -1.) * M
+	slice_matrix!(mc, m, slice, -1.0, mc.s.U)
+	mul!(mc.s.tmp, mc.s.U, M)
+	M .= mc.s.tmp
 	nothing
 end
 function multiply_daggered_slice_matrix_left!(mc::DQMC_CBFalse, m::Model,
 								slice::Int, M::AbstractMatrix)
-	M .= adjoint(slice_matrix(mc, m, slice, 1.)) * M
+	slice_matrix!(mc, m, slice, 1.0, mc.s.U)
+	mul!(mc.s.tmp, adjoint(mc.s.U), M)
+	M .= mc.s.tmp
 	nothing
 end
 
@@ -53,103 +84,141 @@ const DQMC_CBTrue = DQMC{M, CheckerboardTrue} where M
 
 function slice_matrix(mc::DQMC_CBTrue, m::Model, slice::Int,
 					power::Float64=1.)
-  M = eye(heltype(mc), m.flv*m.l.sites)
-  if power > 0
-    multiply_slice_matrix_left!(mc, m, slice, M)
-  else
-    multiply_slice_matrix_inv_left!(mc, m, slice, M)
-  end
-  return M
+    M = Matrix{heltype(mc)}(I, m.flv*m.l.sites, m.flv*m.l.sites)
+  	if power > 0
+    	multiply_slice_matrix_left!(mc, m, slice, M)
+  	else
+    	multiply_slice_matrix_inv_left!(mc, m, slice, M)
+  	end
+  	return M
 end
+function slice_matrix!(mc::DQMC_CBTrue, m::Model, slice::Int,
+					power::Float64=1., M = mc.s.U)
+	copyto!(M, I)
+  	if power > 0
+    	multiply_slice_matrix_left!(mc, m, slice, M)
+  	else
+    	multiply_slice_matrix_inv_left!(mc, m, slice, M)
+  	end
+  	return M
+end
+
 function multiply_slice_matrix_left!(mc::DQMC_CBTrue, m::Model, slice::Int,
                     M::AbstractMatrix{T}) where T<:Number
-  s = mc.s
-  interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, 1.)
+    s = mc.s
+    interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, 1.)
 
-  M[:] = s.eV * M
-  M[:] = s.chkr_mu * M
+	mul!(s.tmp, s.eV, M)
+	M .= s.tmp
+	mul!(s.tmp, s.chkr_mu, M)
+  	M .= s.tmp
 
-  @inbounds @views begin
-    for i in reverse(2:s.n_groups)
-      M[:] = s.chkr_hop_half[i] * M
-    end
-    M[:] = s.chkr_hop[1] * M
-    for i in 2:s.n_groups
-      M[:] = s.chkr_hop_half[i] * M
-    end
-  end
-  nothing
+    @inbounds begin
+    	for i in reverse(2:s.n_groups)
+			mul!(s.tmp, s.chkr_hop_half[i], M)
+			M .= s.tmp
+    	end
+		mul!(s.tmp, s.chkr_hop[1], M)
+		M .= s.tmp
+    	for i in 2:s.n_groups
+			mul!(s.tmp, s.chkr_hop_half[i], M)
+			M .= s.tmp
+    	end
+  	end
+  	nothing
 end
 function multiply_slice_matrix_right!(mc::DQMC_CBTrue, m::Model, slice::Int,
                     M::AbstractMatrix{T}) where T<:Number
-  s = mc.s
-  @inbounds @views begin
-    for i in reverse(2:s.n_groups)
-      M[:] = M * s.chkr_hop_half[i]
-    end
-    M[:] = M * s.chkr_hop[1]
-    for i in 2:s.n_groups
-      M[:] = M * s.chkr_hop_half[i]
-    end
-  end
+    s = mc.s
+  	@inbounds begin
+    	for i in reverse(2:s.n_groups)
+			mul!(s.tmp, M, s.chkr_hop_half[i])
+			M .= s.tmp
+    	end
+		mul!(s.tmp, M, s.chkr_hop[1])
+		M .= s.tmp
+    	for i in 2:s.n_groups
+			mul!(s.tmp, M, s.chkr_hop_half[i])
+			M .= s.tmp
+    	end
+  	end
 
-  interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, 1.)
-  M[:] = M * s.chkr_mu
-  M[:] = M * s.eV
-  nothing
+  	interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, 1.)
+	mul!(s.tmp, M, s.chkr_mu)
+	M .= s.tmp
+	mul!(s.tmp, M, s.eV)
+	M .= s.tmp
+  	nothing
 end
 function multiply_slice_matrix_inv_left!(mc::DQMC_CBTrue, m::Model, slice::Int,
                     M::AbstractMatrix{T}) where T<:Number
-  s = mc.s
-  @inbounds @views begin
-    for i in reverse(2:s.n_groups)
-      M[:] = s.chkr_hop_half_inv[i] * M
-    end
-    M[:] = s.chkr_hop_inv[1] * M
-    for i in 2:s.n_groups
-      M[:] = s.chkr_hop_half_inv[i] * M
-    end
-  end
+    s = mc.s
+  	@inbounds begin
+    	for i in reverse(2:s.n_groups)
+			mul!(s.tmp, s.chkr_hop_half_inv[i], M)
+			M .= s.tmp
+    	end
+		mul!(s.tmp, s.chkr_hop_inv[1], M)
+		M .= s.tmp
+    	for i in 2:s.n_groups
+			mul!(s.tmp, s.chkr_hop_half_inv[i], M)
+			M .= s.tmp
+    	end
+  	end
 
-  interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, -1.)
-  M[:] = s.chkr_mu_inv * M
-  M[:] = s.eV * M
-  nothing
+  	interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, -1.)
+	mul!(s.tmp, s.chkr_mu_inv, M)
+	M .= s.tmp
+	mul!(s.tmp, s.eV, M)
+	M .= s.tmp
+  	nothing
 end
 function multiply_slice_matrix_inv_right!(mc::DQMC_CBTrue, m::Model, slice::Int,
                     M::AbstractMatrix{T}) where T<:Number
-  s = mc.s
-  interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, -1.)
-  M[:] = M * s.eV
-  M[:] = M * s.chkr_mu_inv
+    s = mc.s
+  	interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, -1.)
+	mul!(s.tmp, M, s.eV)
+	M .= s.tmp
+	mul!(s.tmp, M, s.chkr_mu_inv)
+	M .= s.tmp
 
-  @inbounds @views begin
-    for i in reverse(2:s.n_groups)
-      M[:] = M * s.chkr_hop_half_inv[i]
-    end
-    M[:] = M * s.chkr_hop_inv[1]
-    for i in 2:s.n_groups
-      M[:] = M * s.chkr_hop_half_inv[i]
-    end
-  end
-  nothing
+
+  	@inbounds begin
+    	for i in reverse(2:s.n_groups)
+			mul!(s.tmp, M, s.chkr_hop_half_inv[i])
+			M .= s.tmp
+    	end
+		mul!(s.tmp, M, s.chkr_hop_inv[1])
+		M .= s.tmp
+    	for i in 2:s.n_groups
+			mul!(s.tmp, M, s.chkr_hop_half_inv[i])
+			M .= s.tmp
+    	end
+  	end
+  	nothing
 end
 function multiply_daggered_slice_matrix_left!(mc::DQMC_CBTrue, m::Model, slice::Int,
                     M::AbstractMatrix{T}) where T<:Number
-  s = mc.s
-  @inbounds @views begin
-    for i in reverse(2:s.n_groups)
-      M[:] = s.chkr_hop_half_dagger[i] * M
-    end
-    M[:] = s.chkr_hop_dagger[1] * M
-    for i in 2:s.n_groups
-      M[:] = s.chkr_hop_half_dagger[i] * M
-    end
-  end
+    s = mc.s
 
-  interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, 1.)
-  # s.eV == adjoint(s.eV) and s.chkr_mu == adjoint(s.chkr_mu)
-  M[:] = s.chkr_mu * M
-  M[:] = s.eV * M
-  nothing
+  	@inbounds begin
+    	for i in reverse(2:s.n_groups)
+			mul!(s.tmp, s.chkr_hop_half_dagger[i], M)
+			M .= s.tmp
+    	end
+		mul!(s.tmp, s.chkr_hop_dagger[1], M)
+		M .= s.tmp
+    	for i in 2:s.n_groups
+			mul!(s.tmp, s.chkr_hop_half_dagger[i], M)
+			M .= s.tmp
+    	end
+  	end
+
+  	interaction_matrix_exp!(mc, m, s.eV, mc.conf, slice, 1.)
+  	# s.eV == adjoint(s.eV) and s.chkr_mu == adjoint(s.chkr_mu)
+	mul!(s.tmp, s.chkr_mu, M)
+	M .= s.tmp
+	mul!(s.tmp, s.eV, M)
+	M .= s.tmp
+  	nothing
 end
