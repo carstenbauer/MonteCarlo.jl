da7ffa1d9fb889153b04144e02c3d2fec9e67c2a
Frederic Freyer <ffreyer@l45.thp.uni-koeln.de>
2019-09-19 15:29:22 +0200

    change Observable -> LightObservable

--- src/flavors/DQMC/measurements.jl
+++ src/flavors/DQMC/measurements.jl
@@ -7,7 +7,12 @@ struct GreensMeasurement{OT <: AbstractObservable} <: AbstractMeasurement
     obs::OT
 end
 function GreensMeasurement(mc::DQMC, model)
-    o = Observable(typeof(mc.s.greens), "Equal-times Green's function")
+    o = LightObservable(
+        LogBinner(zeros(eltype(mc.s.greens), size(mc.s.greens))),
+        "Equal-times Green's function",
+        "Observables.jld",
+        "Equal-times Green's function"
+    )
     GreensMeasurement{typeof(o)}(o)
 end
 
@@ -31,7 +36,7 @@ struct BosonEnergyMeasurement{OT <: AbstractObservable} <: AbstractMeasurement
     obs::OT
 end
 function BosonEnergyMeasurement(mc::DQMC, model)
-    o = Observable(Float64, "Bosonic Energy")
+    o = LightObservable(Float64, name="Bosonic Energy", alloc=1_000_000)
     BosonEnergyMeasurement{typeof(o)}(o)
 end
 prepare!(::BosonEnergyMeasurement, mc::DQMC, model) = nothing
