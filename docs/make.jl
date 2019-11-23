c8d34876a317dbe5271639c9acf35d733124fae2
Carsten Bauer <crstnbr@gmail.com>
2019-11-23 15:06:48 +0100

    fixing doc build

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, MonteCarlo
 makedocs(modules = [MonteCarlo], doctest=false)
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math", 
+    deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math",
         "pygments", "pymdown-extensions"),
     repo   = "github.com/crstnbr/MonteCarlo.jl.git",
     julia  = "release",
