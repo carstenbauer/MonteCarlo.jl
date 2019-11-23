06ae445d64e8b1bf8f42d95e6ba3ab5d027d130f
Carsten Bauer <crstnbr@gmail.com>
2019-11-23 15:37:42 +0100

    markdown docs fix

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,22 @@
-using Documenter, MonteCarlo
+using Documenter, DocumenterMarkdown, MonteCarlo
 
-makedocs(modules = [MonteCarlo], doctest=false)
+makedocs(
+    modules = [MonteCarlo],
+    doctest = false,
+    sitename = "MonteCarlo.jl",
+    format = Markdown(),
+)
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "mkdocs-material" ,"python-markdown-math",
-        "pygments", "pymdown-extensions"),
-    repo   = "github.com/crstnbr/MonteCarlo.jl.git",
-    make   = () -> run(`mkdocs build`),
+    deps = Deps.pip(
+        "mkdocs",
+        "mkdocs-material",
+        "python-markdown-math",
+        "pygments",
+        "pymdown-extensions",
+    ),
+    repo = "github.com/crstnbr/MonteCarlo.jl.git",
+    make = () -> run(`mkdocs build`),
     target = "site",
-    push_preview=true
+    push_preview = true,
 )
