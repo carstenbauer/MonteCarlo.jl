28a8cb37abeeda43084ff9bd39a00db86954f124
Carsten Bauer <crstnbr@gmail.com>
2019-12-17 23:21:42 +0100

    switch to regular documenter doc style

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,36 @@
-using Documenter, DocumenterMarkdown, MonteCarlo
+using Documenter, MonteCarlo
 
 makedocs(
     modules = [MonteCarlo],
     doctest = false,
     sitename = "MonteCarlo.jl",
-    format = Markdown(),
+    pages = [
+        "Introduction" => "index.md",
+        "Manual" => [
+            "Getting started" => "manual/gettingstarted.md",
+            "Showcase" => "manual/showcase.md",
+        ],
+        "Physical models" => [
+            "Ising model" => "models/ising.md",
+            "Attractive Hubbard model" => "models/hubbardattractive.md",
+        ],
+        "Monte Carlo flavors" => [
+            "MC" => "flavors/mc.md",
+            "DQMC" => "flavors/dqmc.md",
+        ],
+        "Lattices" => "lattices.md",
+        "Customize" => "customize.md",
+        "Interfaces" => [
+            "MC" => "interfaces/MC.md",
+            "DQMC" => "interfaces/DQMC.md",
+        ],
+        "General exports" => ["General" => "methods/general.md"],
+    ],
+    # assets = ["assets/custom.css", "assets/custom.js"]
 )
 
 deploydocs(
-    deps = Deps.pip(
-        "mkdocs",
-        "mkdocs-material",
-        "python-markdown-math",
-        "pygments",
-        "pymdown-extensions",
-    ),
     repo = "github.com/crstnbr/MonteCarlo.jl.git",
-    make = () -> run(`mkdocs build`),
-    target = "site",
     push_preview = true,
+    target = "site",
 )
