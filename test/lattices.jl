da2e20e8a7f436145e0c067911ddb414dbf8a8ae
F Freyer <frederic481994@hotmail.de>
2020-08-05 21:20:11 +0200

    remove Honeycomb lattice (implementation wrong)

--- test/lattices.jl
+++ test/lattices.jl
@@ -52,38 +52,64 @@ for (d, lattype) in enumerate((
     end
 end
 
-@testset "2D Honeycomb" begin
-    for L in (2, 3)
-        l = MonteCarlo.HoneycombLattice(L)
-        @test length(l) == (2L)^2
-
-        bonds = collect(MonteCarlo.neighbors(l, Val(true)))
-        @test length(bonds) == 3 * (2L)^2
-        @test allunique(bonds)
-        for i in 0:length(l)-1
-            # same source
-            @test all(bonds[3i+1][1] == x[1] for x in bonds[3i .+ (2:3)])
-            # different target
-            @test allunique(x[2] for x in bonds[3i .+ (1:3)])
-        end
-
-        reduced_bonds = collect(MonteCarlo.neighbors(l, Val(false)))
-        @test length(reduced_bonds) == round(Int64, 1.5 * (2L)^2)
-        # If directed is false, only one of (i, j) and (j, i)
-        # should be kept.
-        mirrored_bonds = [[trg, src] for (src, trg) in reduced_bonds]
-        all_bonds = vcat(reduced_bonds, mirrored_bonds)
-        @test sort(all_bonds) == sort(bonds)
-
-        # For L < 3 NNN wrap around the lattice
-        L < 3 && continue
-        l = MonteCarlo.HoneycombLattice(L, true)
-        @test size(l.NNNs) == (6, length(l))
-        for i in 1:length(l)
-            @test allunique(l.NNNs[:, i])
-        end
-    end
-end
+# @testset "2D Honeycomb" begin
+#     for L in (2, 3)
+#         l = MonteCarlo.HoneycombLattice(L)
+#         @test length(l) == (2L)^2
+#
+#         bonds = collect(MonteCarlo.neighbors(l, Val(true)))
+#         @test length(bonds) == 3 * (2L)^2
+#         @test allunique(bonds)
+#         for i in 0:length(l)-1
+#             # same source
+#             @test all(bonds[3i+1][1] == x[1] for x in bonds[3i .+ (2:3)])
+#             # different target
+#             @test allunique(x[2] for x in bonds[3i .+ (1:3)])
+#         end
+#
+#         reduced_bonds = collect(MonteCarlo.neighbors(l, Val(false)))
+#         @test length(reduced_bonds) == round(Int64, 1.5 * (2L)^2)
+#         # If directed is false, only one of (i, j) and (j, i)
+#         # should be kept.
+#         mirrored_bonds = [[trg, src] for (src, trg) in reduced_bonds]
+#         all_bonds = vcat(reduced_bonds, mirrored_bonds)
+#         @test sort(all_bonds) == sort(bonds)
+#
+#         # For L < 3 NNN wrap around the lattice
+#         L < 3 && continue
+#         l = MonteCarlo.HoneycombLattice(L, true)
+#         @test size(l.NNNs) == (6, length(l))
+#         for i in 1:length(l)
+#             @test allunique(l.NNNs[:, i])
+#         end
+#
+#         positions = MonteCarlo.positions(l)
+#         mask = MonteCarlo.DistanceMask(l)
+#         @test mask isa MonteCarlo.VerboseDistanceMask
+#         for i in 1:length(l)
+#             @test allunique(mask[i, :])
+#             @test allunique(mask[:, i])
+#         end
+#         wrap = MonteCarlo.generate_combinations(
+#             [L * [0.8660254037844386, -0.5], L * [0.8660254037844386, 0.5]]
+#         )
+#
+#         # in the same direction
+#         dirs = MonteCarlo.directions(mask, l)
+#         for src in 1:length(l)
+#             for (idx, trg) in MonteCarlo.getorder(mask, src)
+#                 d = round.(positions[trg] .- positions[src] .+ wrap[1], digits=6)
+#                 for v in wrap[2:end]
+#                     new_d = round.(positions[trg] .- positions[src] .+ v, digits=6)
+#                     if norm(new_d) < norm(d)
+#                         d .= new_d
+#                     end
+#                 end
+#                 @test dirs[idx] ≈ d
+#             end
+#         end
+#     end
+# end
 
 
 # # TODO: maybe if LatticePhysics gets registered one day?
