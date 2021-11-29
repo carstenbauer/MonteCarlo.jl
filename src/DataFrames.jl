26fc9a1b3418eac0efa35219be9618caf82e666b
ffreyer <frederic481994@hotmail.de>
2021-11-29 16:36:54 +0100

    add replace & always add simulations

--- src/DataFrames.jl
+++ src/DataFrames.jl
@@ -1,23 +1,23 @@
 import .DataFrames
 
-function build_default_getter(whitelist, blacklist)
+function build_default_getter(whitelist, blacklist, replace)
     function default_data(mc)
         d = Dict{Symbol, Any}()
         param = parameters(mc)
         for (k, v) in pairs(param)
             if !(k in blacklist) && (isempty(whitelist) || (k in whitelist))
-                push!(d, k => v)
+                push!(d, replace(k => v))
             end
         end
-        push!(d, :mc => mc)
+        # push!(d, :mc => mc)
         d
     end
 end
 
 function DataFrames.DataFrame(
         mcs::Vector{<: MonteCarloFlavor};
-        whitelist = [], blacklist = [],
-        getter = build_default_getter(whitelist, blacklist)
+        whitelist = [], blacklist = [], replace = identity,
+        getter = build_default_getter(whitelist, blacklist, replace)
     )
 
     df = mcs |> first |> getter |> DataFrames.DataFrame
@@ -26,6 +26,8 @@ function DataFrames.DataFrame(
         data = mcs[i] |> getter
         push!(df, data)
     end
+
+    df.mc = mcs
 
     df
 end
