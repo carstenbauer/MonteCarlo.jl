760c626ea6c1427ecb2fe6ed9679b7dc4cfedfec
ffreyer <frederic481994@hotmail.de>
2021-12-06 10:51:50 +0100

    define interaction term with minus

--- src/models/HubbardModel/HubbardModel.jl
+++ src/models/HubbardModel/HubbardModel.jl
@@ -9,13 +9,15 @@ abstract type HubbardModel <: Model end
 """
     HubbardModel([args..., ]; U[, kwargs...])
 
-Creates an attractive or repulsive Hubbard model.
+Creates an attractive or repulsive Hubbard model. Assumes the interaction to be
+given by -U ∑ᵢ (nᵢ↑ - 0.5) (nᵢ↓ - 0.5), i.e. U > 0 is the attractive Hubbard 
+model.
 """
 function HubbardModel(args...; U, kwargs...)
-    if U > 0.0
-        HubbardModelRepulsive(args..., U = U; kwargs...)
+    if U < 0.0
+        HubbardModelRepulsive(args..., U = -U; kwargs...)
     else
-        HubbardModelAttractive(args..., U = -U; kwargs...)
+        HubbardModelAttractive(args..., U = U; kwargs...)
     end
 end
 
